eb9be7b4dae0463bfad4ea4a1898b938345f3298
Augusto Ribeiro <augustoribeiro@Augustos-MacBook-Pro.local>
2023-09-11 21:08:56 -0300

    Addicionando os testes mockado da base de dados

--- python/main.py
+++ python/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from controller.StockController import router as book_router
-from dotenv import dotenv_values
-from pymongo import MongoClient
 import logging
-
-
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -14,17 +10,9 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-
 @app.get("/")
 def root():
     return {"message": "Welcome  Stock application !"}
-
-
-@app.on_event("shutdown")
-def shutdown_db_client():
-    logger.info("Shutdown the database connection")
-    app.mongodb_client.close()
-
 
 app.include_router(book_router, tags=["stock"], prefix="/stock")
 handler = Mangum(app=app)
